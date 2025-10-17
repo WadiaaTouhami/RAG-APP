ca0b7d0794ed09b71c7bfba93fb870ad9cd8814e
WadiaaTouhami <wadiahtouhemi@gmail.com>
2025-10-17 17:36:11 +0200

    add load_dotenv

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv()
+
+from routes import base
 
 app = FastAPI()
 
-
-@app.get("/welcome")
-def welcome():
-    return {"message": "Hello World!"}
-
-@app.get("/")
-def root():
-    return {"message": "OK"}
+app.include_router(base.base_router)
